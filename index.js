8dad0c8b7c098a4da3b23731be6d68222098397e
Brandon <brandonkanute@gmail.com>
2022-04-03 02:13:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(namearr, event){
+    let thankyou = []
+    for (let i = 0; i < namearr.length; i++) {
+        thankyou.push(`Thank you, ${namearr[i]}, for the wonderful ${event} gift!`)
+    
+    }
+    return thankyou;
+};
+
+console.log(writeCards(["mark","peter","Kamau"],"Roracio"));
+
+function countDown(int){
+    while (int >= 0){
+        console.log(int);
+        int = int -1 ;
+    }
+};
